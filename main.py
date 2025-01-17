dc0430c96828f28b1e08da8bfcd89c7d7700dcab
Orest I <orest58008@yamadachi.quest>
2025-01-17 04:28:58 +0200

    add new categories and aliases
    Signed-off-by: Orest I <orest58008@yamadachi.quest>

--- main.py
+++ main.py
@@ -26,29 +26,26 @@ illusts = map(
     set(map(lambda x: re.sub(r"_p\d\..+", "", x), picfiles))
 )
 
-illust_tags = {}
-
 for (picname, illust) in illusts:
     if illust.illust == None:
-        if illust.error:
+        if illust.error and not illust.error.user_message == "Page not found":
             raise BaseException(illust.error)
-        illust_tags[picname] = "NULL"
-        continue
-    
-    tags = list(map(
-        lambda x: aliases[x] if x in aliases.keys() else x,
-        map(
-            lambda x: x.name if x.translated_name is None else
-            x.translated_name,
-            illust.illust.tags
-        )
-    ))
-
-    category_tags = [t for t in tags if t in categories]
-    if len(category_tags) != 0:
-        category = category_tags[0]
+        category = "NULL"
     else:
-        category = tags[0] if tags else "NULL"
+        tags = list(map(
+            lambda x: aliases[x] if x in aliases.keys() else x,
+            map(
+                lambda x: x.name if x.translated_name is None else
+                x.translated_name,
+                illust.illust.tags
+            )
+        ))
+
+        category_tags = [t for t in tags if t in categories]
+        if len(category_tags) != 0:
+            category = category_tags[0]
+        else:
+            category = tags[0] if tags else "NULL"
         
     category_path = os.path.join(picpath, category)
     if not os.path.exists(category_path):
